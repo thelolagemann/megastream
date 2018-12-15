a75d88144a222e6af2646d6e7b5a56ffe48e212f
thelolagemann <thegamerfromwaitforit@gmail.com>
2018-12-15 23:33:26 +0000

    created error middleware

--- helpers.js
+++ helpers.js
@@ -1,5 +1,11 @@
+const createError = (statusCode, message) => {
+  let err = new Error(message);
+  err.statusCode = statusCode;
+  return err;
+}
+
 const log = (message) => {
   process.env.NODE_ENV == 'development' ? console.log(message) : null;
 };
 
-module.exports = { log };
+module.exports = { createError, log };
